489f6855acc213fbe0d003d534746cd6b66a8189
Olimjon <tajiev0711@gmail.com>
2023-01-16 16:01:25 +0500

    Added Services and Dtos

--- WebCrudFiles/Program.cs
+++ WebCrudFiles/Program.cs
@@ -1,8 +1,20 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using WebCrudFiles.Data;
+using WebCrudFiles.Mappers;
+using WebCrudFiles.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationContext>(opt => opt.UseSqlite(connection));
+
+builder.Services.AddScoped<IFileService,FileService>();
+builder.Services.AddAutoMapper(typeof(EntitiesMap));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
